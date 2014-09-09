a393d1d5e977654dc90f4aa52e6c7db0e2cc6bdd
Tiago Furlanetto <tiago.f.furlanetto@gmail.com>
2014-09-09 20:50:16 -0300

    Initial Commit

---
+++ ScrollViewLayoutBreaking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewLayoutBreaking
+//
+//  Created by Tiago Furlanetto on 9/9/14.
+//  Copyright (c) 2014 Uzaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollViewLayoutBreaking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollViewLayoutBreaking
+//
+//  Created by Tiago Furlanetto on 9/9/14.
+//  Copyright (c) 2014 Uzaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
